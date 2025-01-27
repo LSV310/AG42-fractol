330113925a1f3cd86e441dfb102a81d093c7fe6d
Anicet Gruet <agruet@paul-f4Ar1s3.clusters.42paris.fr>
2025-01-27 15:33:04 +0100

    norminetting

--- libft/srcs/ft_str_equals.c
+++ libft/srcs/ft_str_equals.c
@@ -6,7 +6,7 @@
 /*   By: agruet <agruet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/13 16:03:27 by agruet            #+#    #+#             */
-/*   Updated: 2025/01/13 16:45:43 by agruet           ###   ########.fr       */
+/*   Updated: 2025/01/27 15:28:37 by agruet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,9 @@ int	ft_str_equals(const char *s1, const char *s2)
 		return (0);
 	while (s1[i] && s2[i])
 	{
-		if ((s1[i] != s2[i]) && (ft_isalpha(s1[i]) && (s1[i] != ft_toupper(s2[i])
-			&& s1[i] != ft_tolower(s2[i]))))
+		if ((s1[i] != s2[i]) && (ft_isalpha(s1[i])
+				&& (s1[i] != ft_toupper(s2[i])
+					&& s1[i] != ft_tolower(s2[i]))))
 			return (0);
 		i++;
 	}
